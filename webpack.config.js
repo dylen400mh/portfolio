71d20c1d27a9e0e7bfd79d56d0df6358962afff2
Dylen <dylenbelanger2004@gmail.com>
2023-08-23 22:11:27 -0400

    Add html-loader to rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/js/index.js",
-    plugins: [
-        new HtmlWebpackPlugin({
-          title: 'Dylen Belanger',
-          template: "./src/html/template.html"
-        }),
-      ],
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    devtool: 'inline-source-map',
+  entry: "./src/js/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Dylen Belanger",
+      template: "./src/html/template.html",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name]-[hash][ext]",
+        },
+      },
+    ],
+  },
+  devtool: "inline-source-map",
 };
